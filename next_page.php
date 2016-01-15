5024d62915418ede8841e226290859f2fb8e6799
Mushahid Hassan <mushahidhassan110@gmail.com>
2016-01-15 23:06:46 +0500

    Create next_page.php

---
+++ next_page.php
@@ -0,0 +1,3 @@
+$fname = $_POST['f_name']; // this ($fname) variable will contain the value from input field First Name
+$lname = $_POST['l_name']; // this (lfname) variable will contain the value from input field Last Name
+echo $fname; // echo funciton will print the value that you send in $fname variable
